a57b432c3ed5bb79df8d078da05e0d2244372d66
채승지 <69447192+ChaeSeungJi@users.noreply.github.com>
2023-05-16 12:53:39 +0900

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
-df = pd.DataFrame(np.random.randn(500,2) / [50,50] + [37.76,-122.4], columns=(['lat','lot']))
+df = pd.DataFrame(np.random.randn(500,2) / [50,50] + [37.76,-122.4], columns=['lat','lot'])
+
 st.map(df)
 
 rand = np.random.normal(1,2,size=20)
